7dd55beb6437cb52dd8f7ae865fce09b9b10c081
GSTEPS\Hanokh.Aloni <hanokhaloni@gmail.com>
2017-06-11 14:31:57 +0300

    initial commit (up to slide 4 complete)

---
+++ 1_of_n/UnitTestProject1/Calculator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace UnitTestProject1
+{
+    internal class Calculator
+    {
+        private const string CUSTOM_PREFIX = "//";
+        private static string CUSTOM_AFTER_PREFIX = "\n";
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="commaSeperatedNumbers"></param>
+        /// <returns></returns>
+        internal static int Add(string commaSeperatedNumbers)
+        {
+            var defaultDelimiter = ',';
+            //TODO extract "returnZeroForEmptyString
+            if (String.IsNullOrEmpty(commaSeperatedNumbers))
+            {
+                return 0;
+            }
+
+            if (commaSeperatedNumbers.StartsWith(CUSTOM_PREFIX))
+            {
+                defaultDelimiter = commaSeperatedNumbers.Substring(CUSTOM_PREFIX.Length, CUSTOM_PREFIX.Length + 1).ToCharArray()[0];
+                commaSeperatedNumbers = commaSeperatedNumbers.Substring(
+                    CUSTOM_PREFIX.Length + CUSTOM_AFTER_PREFIX.Length + 1, 
+                    commaSeperatedNumbers.Length - CUSTOM_PREFIX.Length - CUSTOM_AFTER_PREFIX.Length - 1);
+            }
+
+            var values = commaSeperatedNumbers.Split(defaultDelimiter, '\n');
+
+            return SumValues(values);
+        }
+
+        private static int SumValues(string[] values)
+        {
+            int sum = 0;
+            foreach (var val in values)
+            {
+                sum += int.Parse(val);
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ 1_of_n/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void Add_EmptyString_ReturnZero()
+        {
+            var result = Calculator.Add("");
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Add_One_ReturnOne()
+        {
+            var result = Calculator.Add("1");
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Add_OneAndTwo_ReturnThree()
+        {
+            var result = Calculator.Add("1,2");
+
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void Add_UnknownAmountOfNumbers_ReturnSum1()
+        {
+            var result = Calculator.Add("1,2,3");
+
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        public void Add_UnknownAmountOfNumbers_ReturnSum2()
+        {
+            var result = Calculator.Add("1,6,1,1");
+
+            Assert.AreEqual(9, result);
+        }
+
+        [TestMethod]
+        public void Add_NewlineSeperatorAndAComma_ReturnSum()
+        {
+            var result = Calculator.Add("1\n2,3");
+
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void Add_OneCommaNewLine_ShouldBeInvalid()
+        {
+            var result = Calculator.Add("1,\n");
+        }
+
+        [TestMethod]
+        public void Add_TwoNumbersWithCustomDelimiter_ShouldReturnSum3()
+        {
+            var result = Calculator.Add("//;\n1;2");
+
+            Assert.AreEqual(3, result);
+        }
+    }
+}
